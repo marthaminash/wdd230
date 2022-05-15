76f12f49d64503b7598d47f853d3b330d1177bcd
marthaminash <min22001@byui.edu>
2022-05-14 22:37:06 -0400

    lesson 4

---
+++ lesson 4/chamber/js/script.js
@@ -0,0 +1,18 @@
+let latestDate = new Date(document.lastModified).toLocaleString();
+document.getElementById('placeholder').innerHTML = "Last Updated: " + latestDate;
+
+let date = new Date(year, month, day);
+const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = weekDays[date.getDay()];
+
+const monthArray = ["January","February","March","April","May","June","July",
+"August","September","October","November","December"];
+let currentMonth = monthArray[date.getMonth()];
+
+let currentDate = date.getDate();
+let currentYear = date.getYear();
+
+
+let dateParagraph = `${day}, ${currentDate} ${currentMonth} ${currentYear}`;
+
+document.getElementById('dateParagraph').innerHTML = "Hi";
