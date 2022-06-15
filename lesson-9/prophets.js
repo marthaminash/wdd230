08921f8fc909a2323ead90439ffd9c3208f2c425
Martha Minash <103990217+marthaminash@users.noreply.github.com>
2022-06-15 15:21:45 -0400

    lesson 9 part 1

--- lesson-9/prophets.js
+++ lesson-9/prophets.js
@@ -1,36 +1,39 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 const cards = document.querySelector('.cards');
 
-fetch(requestURL)
-    .then(function (response) {
-    return response.json();
+async function getProphets(){
+    let response = await fetch(requestURL)
+    if(response.ok){
+        let data = await response.json();
+        buildPhrophetCards(data)
+    } else {
+        throw Error(response.status);
+    }
+}
+
+function buildPhrophetCards(data){
+    data.prophets.forEach(prophet => {
+        let card = document.createElement('section')
+        let h2 = document.createElement('h2');
+        let img = document.createElement('img');
+        let firstParagraph = document.createElement('p');
+        let secondParagraph = document.createElement('p');
+
+        h2.innerHTML = `${prophet.name} ${prophet.lastname}`;
+        firstParagraph.innerHTML = `Date of birth: ${prophet.birthdate}`;
+        secondParagraph.innerHTML = `Place of birth: ${prophet.birthplace}`;
+        img.setAttribute('src', `${prophet.imageurl}`);
+        img.setAttribute('alt', `${prophet.name} ${prophet.lastname}`);
+        img.setAttribute('width', '200');
+        img.setAttribute('loading', 'lazy')
+
+        card.appendChild(h2);
+        card.appendChild(firstParagraph);
+        card.appendChild(secondParagraph);
+        card.appendChild(img);
+        
+        cards.appendChild(card);
     })
-    .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    prophets.forEach(displayProphets);
-    });
+}
 
-const prophets = jsonObject['prophets'];
-
-
-function displayProphets(prophet) {
-    // Create elements to add to the document
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let portrait = document.createElement('img');
-
-    // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = prophet.name + ' ' + prophet.lastname;
-
-    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    ____.setAttribute('src', prophet.imageurl);
-    ____.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
-    ____.setAttribute('loading', 'lazy');
-
-    // Add/append the section(card) with the h2 element
-    card.appendChild(h2);
-    card.appendChild(portrait);
-
-    // Add/append the existing HTML div with the cards class with the section(card)
-    document.querySelector('div.cards').appendChild(card);
-};
+getProphets()
