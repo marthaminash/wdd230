6e87f6cecd837c0b6d634b1a6db4fd4a02ba99ca
Martha Minash <103990217+marthaminash@users.noreply.github.com>
2022-06-17 21:58:59 -0400

    lesson 9 part 2

--- lesson-9/directory-page/directory.js
+++ lesson-9/directory-page/directory.js
@@ -1,40 +1,50 @@
-const requestURL = './directory.json';
+
 const cards = document.querySelector('.cards');
-console.log(requestURL)
 
-async function getCompanies(){
-    let response = await fetch(requestURL)
-    if(response.ok){
-        let data = await response.json();
-        buildCompanyCards(data)
-    } else {
-        throw Error(response.status);
-    }
-}
-
-function buildCompanyCards(data){
-    data.companies.forEach(company => {
-        let card = document.createElement('section')
-        let h2 = document.createElement('h2');
-        let img = document.createElement('img');
-        let firstParagraph = document.createElement('p');
-        let secondParagraph = document.createElement('p');
-
-        h2.innerHTML = `${company.name}`;
-        firstParagraph.innerHTML = `Address: ${company.address}`;
-        secondParagraph.innerHTML = `Phone number: ${company.phonenumber}`;
-        img.setAttribute('src', `${company.logo}`);
-        img.setAttribute('alt', `${company.name}`);
-        img.setAttribute('width', '100');
-        img.setAttribute('loading', 'lazy')
-
-        card.appendChild(h2);
-        card.appendChild(firstParagraph);
-        card.appendChild(secondParagraph);
-        card.appendChild(img);
-        
-        cards.appendChild(card);
+fetch("./directory.json")
+    .then(function (response) {
+        return response.json();
     })
-}
+    .then(function (jsonObject) {
+        console.table(jsonObject);  // temporary checking for valid response and data parsing
+        const companies = jsonObject["companies"];
+        companies.forEach(displayCompanies);
+    });
 
-getCompanies()
+
+    function displayCompanies(company) {
+        //Create elements to add to the document
+        let card = document.createElement("section");
+        let h2 = document.createElement("h2");
+        let address = document.createElement("p");
+        let phone = document.createElement("p");
+        let membership = document.createElement("p");
+        let logoPicture = document.createElement("img");
+    
+        //Change the textContent property of the h2 element to contain the prophet's full name
+        
+        h2.textContent = `${company.name}`;
+        address.innerHTML = `Address: ${company.address}`;
+        phone.innerHTML = `Phone number: ${company.phonenumber}`;
+        membership.innerHTML = `Membership: ${company.membershipLevel}`;
+    
+    
+    
+        //Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+        logoPicture.setAttribute("src", company.logo);
+        logoPicture.setAttribute("alt",`${company.name} logo`);
+        logoPicture.setAttribute("width", "150")
+        logoPicture.setAttribute("loading", "lazy");
+    
+        card.appendChild(h2);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(membership);
+        card.appendChild(logoPicture);
+    
+        // Add/apend the existing HTML div with the cards class with the section(card)
+        document.querySelector("div.cards").appendChild(card);
+    }
+    
+
+
